5ac7ab7a84b7486d40738e0d21b969fcbe26ca2b
Jessica Mitchem <jpmitchem@gmail.com>
2015-02-06 21:13:21 -0500

    sort three inputs

---
+++ 3-8.py
@@ -0,0 +1,10 @@
+__author__ = 'Jessica'
+
+
+num1 = int(raw_input("Please enter a number:"))
+num2 = int(raw_input("Please enter another number:"))
+num3 = int(raw_input("Please enter a third number:"))
+
+print sorted([num1, num2, num3])
+
+
